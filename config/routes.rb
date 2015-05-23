7af8ec9877bcd7cc513e34575ad52e4df0d5acd4
Ji Lee <gafrong@gmail.com>
2015-05-23 19:11:21 +1000

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   get '/', to: redirect('/')
 
-  namespace :api, defaults: {format: :json} do
-    namespace :v1 do
-
-    end
-  end
+  # namespace :api, defaults: {format: :json} do
+  #   namespace :v1 do
+  #     get 'name' => 'name#index', :as => 'name'
+  #   end
+  # end
 
 end
